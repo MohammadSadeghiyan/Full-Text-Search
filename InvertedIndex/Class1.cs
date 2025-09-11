7159509fc25f32fb38ed2d629e0bb1e40651a173
mohammadsadeghian <133510008+MohammadSadeghiyan@users.noreply.github.com>
2025-09-11 12:30:28 +0330

    Initial commit for full-text search project

---
+++ InvertedIndex/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using Porter2Stemmer;
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+namespace InvertedIndex;
+
+
+public class InvertedIndexProcess
+{
+    public static List<string> Spilit(string content)
+    {
+        return Regex.Split(content, @"\s+").ToList();
+    }
+    public static string CleanWord(string fileContent)
+    {
+        string w = Regex.Replace(fileContent, @"[^a-zA-Z0-9#]", "");
+
+        w = w.ToUpper();
+
+        return w;
+    }
+
+    public static List<string> DeleteStopWords(List<string> allWords)
+    {
+        List<string> stopWords = new List<string> { "I", "WE", "YOU", "HE", "SHE", "IT", "THEY", "ME", "HIM", "HER", "US", "THEM" };
+
+        List<string> filteredWords = new List<string>();
+        foreach (string word in allWords)
+        {
+            if (!stopWords.Contains(word) && word != "")
+            {
+                filteredWords.Add(word);
+            }
+        }
+
+        return filteredWords;
+    }
+
+
+    public static List<string> WordStiming(List<string> allWords)
+    {
+        EnglishPorter2Stemmer stemmer = new EnglishPorter2Stemmer();
+        List<string> allRoot = new List<string>();
+        foreach (string word in allWords)
+        {
+            allRoot.Add(stemmer.Stem(word).Value);
+        }
+        return allRoot;
+    }
+
+    public static string WordStiming(string word)
+    {
+        EnglishPorter2Stemmer stemmer = new EnglishPorter2Stemmer();
+
+
+        return stemmer.Stem(word).Value;
+
+    }
+    public static void BuildInvertedIndex(ref Dictionary<string, List<string>> invertedIndex, List<string> allWord, string fileName)
+    {
+        foreach (var word in allWord)
+        {
+            if (invertedIndex.ContainsKey(word))
+            {
+                if (!invertedIndex[word].Contains(fileName))
+                    invertedIndex[word].Add(fileName);
+            }
+            else
+            {
+                invertedIndex.Add(word, new List<string> { fileName });
+            }
+        }
+    }
+}
---
+++ console/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using InvertedIndex;
+namespace FullTextSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> invertedIndex = new Dictionary<string, List<string>>();
+            string folderPath = "/home/mohammad/dotnet/full-text-search/search_data/data/EnglishData";
+            string[] files = Directory.GetFiles(folderPath);
+            string content;
+            List<string> allWord = new List<string>();
+            foreach (string file in files)
+            {
+                //read file
+                content = File.ReadAllText(file);
+                List<string> rawWords = Tokenization.Spilit(content);
+                //tokenization
+                foreach (string word in rawWords)
+                {
+                    allWord.Add(Tokenization.CleanWord(word));
+
+                }
+
+                allWord = Tokenization.DeleteStopWords(allWord);
+
+                allWord = Tokenization.WordStiming(allWord);
+
+                Tokenization.BuildInvertedIndex(ref invertedIndex, allWord, file);
+                allWord.Clear();
+            }
+
+            Console.Write("please enter your string that you want to make full text search on it (hey):");
+            string input = Console.ReadLine();
+            List<string> parts = Regex.Split(input, @"\s+").ToList();
+
+            HashSet<string> onlyParts = new HashSet<string>();
+            HashSet<string> excludeParts = new HashSet<string>();
+            HashSet<string> atLeastParts = new HashSet<string>();
+            List<string> allParts = Tokenization.WordStiming(parts);
+
+            foreach (var part in allParts)
+            {
+                if (part.StartsWith("+"))
+                {
+                    string key = part.Substring(1);
+                    if (invertedIndex.ContainsKey(key))
+                        atLeastParts.UnionWith(invertedIndex[key]);
+                }
+                else if (part.StartsWith("-"))
+                {
+                    string key = part.Substring(1);
+                    if (invertedIndex.ContainsKey(key))
+                        excludeParts.UnionWith(invertedIndex[key]);
+                }
+                else
+                {
+                    string key = part;
+                    if (invertedIndex.ContainsKey(key))
+                        onlyParts.UnionWith(invertedIndex[key]);
+                }
+            }
+
+
+            onlyParts.ExceptWith(excludeParts);
+            if (atLeastParts.Count > 0)
+                onlyParts.IntersectWith(atLeastParts);
+
+            foreach (var doc in onlyParts)
+            {
+                Console.WriteLine(doc);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
